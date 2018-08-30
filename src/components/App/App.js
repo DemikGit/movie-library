00211abeb6de292076275718204e278888c8dabb
Alexander Dyumin <AlekGit@yandex.ru>
2018-08-30 12:00:34 +0300

    adjust app skeleton

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+export class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <CssBaseline />
+        Hello World!
+      </Fragment>
+    );
+  }
+}
+
