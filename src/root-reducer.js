224526589868a88c51fcc12a12f947ba204b7bc8
Alexander Dyumin <AlekGit@yandex.ru>
2018-09-02 09:14:10 +0300

    misc

--- src/root-reducer.js
+++ src/root-reducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import { appReducer as app } from './components/App/AppReducer';
+import { filterReducer as filter } from './components/Filter/FilterReducer';
+import { moveListReducer as movieList } from './components/MovieList/MoveListReducer';
 
 export const rootReducer = combineReducers({
   app,
+  filter,
+  movieList,
 });
